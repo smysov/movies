39abd93af6d6143e6818b7a28df8238b31b2aa39
Sergey Mysov <s.mysov@list.ru>
2021-06-04 20:31:20 +0400

    implemented form of search movies

---
+++ src/components/form/SubmitButton.jsx
@@ -0,0 +1,9 @@
+function SubmitButton() {
+  return (
+    <div className="search__group">
+      <button className="search__button">Search</button>
+    </div>
+  );
+}
+
+export default SubmitButton;
--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,5 +1,15 @@
+import Form from '../Form';
+
 function Main() {
-  return <main className="main-content"></main>;
+  return (
+    <main className="main-content">
+      <section className="search-movies">
+        <div className="container">
+          <Form />
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Main;
